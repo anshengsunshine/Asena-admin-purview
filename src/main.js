516e58df06904e51004fd6353f6d283edc858160
jiajing <jiajing@zhiweidata.com>
2022-02-24 15:58:37 +0800

    main.js中引入部分插件进行使用

--- src/main.js
+++ src/main.js
@@ -2,5 +2,21 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// Mock.js
+import './mock/index.js';  // 该项目所有请求使用mockjs模拟
+// normalize.css
+import 'normalize.css';
+// ant-design-vue
+import 'ant-design-vue/dist/antd.css';
+import { Button, Tag, Pagination, Input, Form, Menu, Layout, Empty } from 'ant-design-vue'
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App);
+app.use(Button)
+    .use(Tag)
+    .use(Pagination)
+    .use(Input)
+    .use(Form)
+    .use(Menu)
+    .use(Layout)
+    .use(Empty)
+app.use(store).use(router).mount("#app");
