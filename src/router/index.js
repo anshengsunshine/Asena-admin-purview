57ba5634394d693ac5dce3d49e021bc8dc5b9fc7
anshengsunshine <646143658@qq.com>
2022-02-26 20:18:22 +0800

    修改home首页展示;新增vue.config.js文件做配置;router.js调整布局路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
 
 const routes = [
   {
@@ -14,17 +13,25 @@ const routes = [
   },
   {
     path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    name: "Index",
+    redirect: "/home",
+    component: () => import(/* webpackChunkName: "home" */"@/views/layout/Layout.vue"),
+    children: [
+      {
+        path: "/home",
+        name: "Home",
+        component: () => import(/* webpackChunkName: "home" */"@/views/home/Home.vue")
+      },
+      {
+        path: "/about",
+        name: "About",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+          import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      },
+    ]
   },
 ];
 
--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-layout>
+  <a-layout class="min_h_100">
     <a-layout-sider
       breakpoint="lg"
       collapsed-width="0"
@@ -31,9 +31,6 @@
           <header-comp />
       </a-layout-header>
       <a-layout-content :style="{ margin: '24px 16px 0' }">
-        <div :style="{ padding: '24px', background: '#fff', minHeight: '360px' }">
-          content
-        </div>
         <router-view />
       </a-layout-content>
       <a-layout-footer style="text-align: center">
